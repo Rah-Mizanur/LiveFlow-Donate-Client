d9cfc035e2d9b3a9f3216295c8fabce9f3701c56
unknown <mijan90786@gmail.com>
2025-12-10 01:26:28 +0600

    start Project with firebase auth

---
+++ src/routes/Routes.jsx
@@ -0,0 +1,27 @@
+import Home from '../pages/Home/Home'
+import ErrorPage from '../pages/ErrorPage'
+import Login from '../pages/Login/Login'
+import SignUp from '../pages/SignUp/SignUp'
+
+
+import MainLayout from '../layouts/MainLayout'
+
+import { createBrowserRouter } from 'react-router'
+
+export const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <MainLayout />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        path: '/',
+        element: <Home />,
+      },
+   
+    ],
+  },
+  { path: '/login', element: <Login /> },
+  { path: '/signup', element: <SignUp /> },
+
+])
